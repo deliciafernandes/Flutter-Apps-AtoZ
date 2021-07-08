bfd202f59795f36031ed7825b9805a990ee8397e
Delicia Fernandes <fernandesdelicia07@gmail.com>
2021-07-08 22:44:45 +0530

    i_am_rich app update

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 
+// Main function is the starting point for all flutter apps
 void main() {
   runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("I am rich"),
-            backgroundColor: Colors.blueGrey[900],
-          ),
-          body: Center(
-            child: Image(
-//            image: NetworkImage('https://cdn.images.express.co.uk/img/dynamic/151/590x/Black-hole-picture-please-time-date-first-image-black-hole-event-horizon-telescope-1112295.jpg?r=1554889792953'),
-              image: AssetImage('images/diamond.png'),
-              ),
-          ),
-          backgroundColor: Colors.blueGrey[500],
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[500],
+        appBar: AppBar(
+          title: Text("I am rich"),
+          backgroundColor: Colors.blueGrey[900],
         ),
+        body: Center(
+          child: Image(
+//            image: NetworkImage('https://cdn.images.express.co.uk/img/dynamic/151/590x/Black-hole-picture-please-time-date-first-image-black-hole-event-horizon-telescope-1112295.jpg?r=1554889792953'),
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
     ),
   );
 }
