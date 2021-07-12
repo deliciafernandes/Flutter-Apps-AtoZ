0ebf2173184f11f79bdd6db824d85effa355b1f3
Delicia Fernandes <fernandesdelicia07@gmail.com>
2021-07-12 23:25:25 +0530

    Update

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,119 +1,138 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MyApp(),
-  );
+const kUrl = 'https://avatars.githubusercontent.com/u/53268119?v=4';
+
+void main() => runApp(MyApp());
+
+class DelsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: SafeArea(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.transparent,
+                backgroundImage: NetworkImage(kUrl),
+              ),
+              Text(
+                'Delicia Fernandes',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'DEVELOPER',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.white,
+                  fontFamily: 'Playfair',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.purple[100],
         body: SafeArea(
-//          child: Row(
-//            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//            children: <Widget>[
-//              Container(
-//                color: Colors.red,
-//                height: double.infinity,
-//                width: 100,
-//              ),
-//              Column(
-//                mainAxisAlignment: MainAxisAlignment.center,
-//                children: <Widget>[
-//                  Container(
-//                    //Single child widget
-//                    color: Colors.yellow,
-//                    height: 100,
-//                    width: 100,
-//                  ),
-//                  Container(
-//                    //Single child widget
-//                    color: Colors.green,
-//                    height: 100,
-//                    width: 100,
-//                  ),
-//                ],
-//              ),
-//              Container(
-//                //Single child widget
-//                color: Colors.blue,
-//                height: double.infinity,
-//                width: 100,
-//              )
-//            ],
-//          ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              backgroundImage: AssetImage("images/del.jpg"),
-              radius: 70,
-            ),
-            Text(
-              "Delicia Fernandes",
-              style: TextStyle(
-                fontSize: 40,
-                fontFamily: "Pacifico",
-                color: Colors.black,
-              ),
-            ),
-            Text(
-              "Software Developer",
-              style: TextStyle(
-                fontSize: 20,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-                fontFamily: "Playfair",
-                color: Colors.deepPurple[900],
-              ),
-            ),
-            SizedBox(
-              width: 150.0,
-              height: 30.0,
-              child: Divider(
-                color: Colors.purple,
-              ),
-            ),
-            Card(
-              //cards by default are white
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.phone,
-                  color: Colors.black,
-                  size: 20.0,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(kUrl),
+                  // backgroundImage: AssetImage("images/del.jpg"),
                 ),
-                title: Text(
-                  "+91 123 456 789",
+                Text(
+                  "Delicia Fernandes",
                   style: TextStyle(
-                    fontSize: 20.0,
+                    fontSize: 40,
+                    fontFamily: "Pacifico",
                     color: Colors.black,
                   ),
                 ),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.email,
-                  color: Colors.black,
-                  size: 20.0,
-                ),
-                title: Text(
-                  "fernandesdelicia07@student.sfit.ac.in",
+                Text(
+                  "Software Developer",
                   style: TextStyle(
-                    fontSize: 15.2,
-                    color: Colors.black,
+                    fontSize: 20,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Playfair",
+                    color: Colors.deepPurple[900],
                   ),
                 ),
-              ),
-            ),
-          ]),
+                SizedBox(
+                  width: 150.0,
+                  height: 30.0,
+                  child: Divider(
+                    color: Colors.purple,
+                  ),
+                ),
+                // Container(
+                //   width: 150.0,
+                //   padding: EdgeInsets.symmetric(vertical: 15.0),
+                //   child: Divider(color: Colors.purple),
+                // ),
+                ReusableCardWidget(
+                  icon: Icons.phone,
+                  text: '+91 123 456 789',
+                  textSize: 20.0,
+                ),
+                ReusableCardWidget(
+                  icon: Icons.email,
+                  text: 'fernandesdelicia07@student.sfit.ac.in',
+                  textSize: 15.0,
+                ),
+              ]),
+        ),
+      ),
+    );
+  }
+}
+
+class ReusableCardWidget extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final double textSize;
+
+  ReusableCardWidget(
+      {@required this.icon, @required this.text, @required this.textSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      //cards by default are white
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+      child: ListTile(
+        leading: Icon(
+          icon,
+          color: Colors.black,
+          size: 20.0,
+        ),
+        title: Text(
+          text,
+          style: TextStyle(
+            fontSize: textSize,
+            color: Colors.black,
+          ),
         ),
       ),
     );
